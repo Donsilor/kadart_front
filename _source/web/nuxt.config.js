2a60827300d7fc909b3dd8665fe8e30e226304bc
gaopeng <763429951@qq.com>
2020-01-09 10:12:19 +0800

    fix

--- _source/web/nuxt.config.js
+++ _source/web/nuxt.config.js
@@ -1,38 +1,58 @@
 module.exports = {
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: 'vue-web2',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Nuxt.js project'
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }]
   },
   /*
-  ** Customize the progress bar color
-  */
-  loading: { color: '#3B8070' },
+   ** Customize the progress bar color
+   */
+  loading: {
+    color: '#3B8070'
+  },
 
   modules: [
-      '@nuxtjs/axios'
+    '@nuxtjs/axios'
   ],
   axios: {
-      // prefix: '/api/',
-      // proxy: true // Can be also an object with default options
+    // prefix: '/api/',
+    // proxy: true // Can be also an object with default options
   },
 
   proxy: {
-      // '/api/': { target: '47.75.100.16', pathRewrite: {'^/api/': ''} }
+    // '/api/': { target: '47.75.100.16', pathRewrite: {'^/api/': ''} }
   },
-  plugins: [
-    {src: '~plugins/ElementUI',ssr: true},
-    {src: "~/plugins/vue-swiper.js",ssr: false},
-    { src: '~/plugins/axios', ssr: true },
+  plugins: [{
+      src: '~plugins/ElementUI',
+      ssr: true
+    },
+    {
+      src: "~/plugins/vue-swiper.js",
+      ssr: false
+    },
+    {
+      src: '~/plugins/axios',
+      ssr: true
+    },
   ],
   css: [
     'element-ui/lib/theme-chalk/index.css',
@@ -41,13 +61,16 @@ module.exports = {
     'swiper/dist/css/swiper.css'
   ],
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** Run ESLint on save
-    */
-    extend (config, { isDev, isClient }) {
+     ** Run ESLint on save
+     */
+    extend(config, {
+      isDev,
+      isClient
+    }) {
       // if (isDev && isClient) {
       //   config.module.rules.push({
       //     enforce: 'pre',
@@ -57,6 +80,16 @@ module.exports = {
       //   })
       // }
     },
-    vendor:['element-ui']
+    vendor: ['element-ui']
+  },
+  router: {
+    extendRoutes(routes) {
+      routes.push({
+        name: 'goods-detail',
+        path: '/goods-detail',
+        component: 'pages/goods-detail.vue',
+        alias: '/goods_:goodName/:id'
+      })
+    }
   }
 }
